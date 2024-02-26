952ec310654afa9262d61544a19a5b35fb69ffd9
malyshkinakb <malyshkinakb@gmail.com>
2024-02-27 01:12:09 +0300

    Добавляет изменения

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,12 @@
 /* в этот файл добавляет скрипты*/
+const slider = document.getElementById('slider');
+
+noUiSlider.create(slider, {
+  start: [20, 80],
+  connect: true,
+  range: {
+    'min': 0,
+    'max': 100
+  }
+});
+
